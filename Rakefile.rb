d86af24f0225cec2ccc1eab0b791b762ff5db102
Ethan <ethan@unth.net>
2025-05-21 12:52:26 -0700

    gig

---
+++ Rakefile.rb
@@ -0,0 +1,9 @@
+require 'gig'
+
+ignore_files = %w(
+  .gitignore
+  Gemfile
+  Rakefile.rb
+).map { |glob| Dir.glob(glob, File::FNM_DOTMATCH) }.inject([], &:|)
+
+Gig.make_task(gemspec_filename: 'jsi_util_linux.gemspec', ignore_files: ignore_files)
